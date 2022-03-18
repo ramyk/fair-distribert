527cb09f82f462cbf41853da9853446586846ead
ramyk <ramy.kader.rk@gmail.com>
2022-03-18 23:24:22 +0100

    config bert finetuning

---
+++ configs/bert_mrpc.py
@@ -0,0 +1,23 @@
+from os.path import join
+
+
+EXP_NAME    = "BERT_MRPC"
+SEED        = 4321  # REPRODUCIBILITY
+
+# REPOSITORIES
+WORKSPACE   = "/home/ubuntu/interview/"
+SNAP_REPO   = join(WORKSPACE, "snapshots/")
+MODL_REPO   = join(WORKSPACE, "weights/")  
+LOGS_REPO   = join(WORKSPACE, "logs/")
+
+# MODEL PARAMS
+CHECKPOINT  = "bert-base-uncased"
+MAX_LENGTH  = 256
+
+# TRAINING PARAMS
+TRAIN_BATCH_SIZE    = 16
+EVAL_BATCH_SIZE     = 32
+EPOCHS              = 5
+SCHEDULER           = "linear"
+WARMUP_STEPS        = 0
+LEARNING_RATE       = 5e-5
